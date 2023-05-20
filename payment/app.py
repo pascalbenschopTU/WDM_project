ff5f3dc0359f1091cca55cc257c212a24a9fedb6
Leon Kempen <l.m.kempen@outlook.com>
2023-05-20 19:22:52 +0200

    WIP implementing mongo (db works) [no-ci]

--- payment/app.py
+++ payment/app.py
@@ -12,78 +12,71 @@ hostname = os.environ['MONGODB_HOSTNAME']
 database = os.environ['MONGODB_DATABASE']
 gateway_url = os.environ['GATEWAY_URL']
 
-app.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27018/' + os.environ['MONGODB_DATABASE']
+app.config["MONGO_URI"] = f"mongodb://{os.environ['MONGODB_USERNAME']}:{os.environ['MONGODB_PASSWORD']}@{os.environ['MONGODB_HOSTNAME']}:27017/{os.environ['MONGODB_DATABASE']}"
 mongo = PyMongo(app)
 db = mongo.db
-
-
+user_collection = db.users
+paid_order_colleciton = db.paid_orders
 
 @app.post('/create_user')
 def create_user():
-    print("did this", file=sys.stderr)
-    collection = db.users
-    user = collection.insert_one({'credit': 0})
-    return {'user_id': 0}
+    user = user_collection.insert_one({'credit': 0}) 
     user_id = user.inserted_id
-    return {'user_id': user_id}
+    return {'user_id': str(user_id)}
 
 
 
 @app.get('/find_user/<user_id>')
 def find_user(user_id: str):
-    user = db.hmget(f'user:{user_id}', 'credit')
+    user = user_collection.find_one({'_id': ObjectId(user_id)})
     if None in user:
         return {'Error': 'User not found'}, 404
-    return {"user_id": int(user_id), "credit": int(user[0])}, 200
+    return {'user_id': user_id, 'credit': user['credit']}, 200
 
 
 @app.post('/add_funds/<user_id>/<amount>')
 def add_credit(user_id: str, amount: int):
     if int(amount) < 0:
         return {'Error': 'Amount must be positive'}, 400
-    if not db.exists(f'user:{user_id}'):
-        return {'Error': 'User not found'}, 400
-    db.hincrby(f'user:{user_id}', 'credit', int(amount))
-    return 'Success', 200
+    user = user_collection.find_one_or_404({'_id': ObjectId(user_id)})
+    credit: int = int(user['credit'])
+    user['credit'] = credit + int(amount)
+    user_collection.update_one(user)
 
 
 @app.post('/pay/<user_id>/<order_id>/<amount>')
 def remove_credit(user_id: str, order_id: str, amount: int):
-    credit = db.hget(f'user:{user_id}', 'credit')
-    if int(credit) < int(amount):
+    user = user_collection.find_one_or_404({'_id': ObjectId(user_id)})
+    if user['credit'] < int(amount):
         return 'Not enough credit', 400
-    order = db.exists(f'paid_orders:{order_id}')
+    
+    order = paid_order_colleciton.find_one({'order_id': order_id})
 
     if order:
         return 'Order already paid', 401
 
-    p = db.pipeline(transaction=True)
-    p.hincrby(f'user:{user_id}', 'credit', -int(amount))
-    p.hmset(f'paid_orders:{order_id}', {
-            'amount_paid': amount, 'user_id': user_id})
-    p.execute()
+    # Update to transaction
+    user['credit'] -= amount
+    user_collection.update_one(user)
+    paid_order_colleciton.insert_one({'order_id': order_id, 'amount': amount}) 
     return 'Success', 200
 
 
 @app.post('/cancel/<user_id>/<order_id>')
 def cancel_payment(user_id: str, order_id: str):
-    order = db.hmget(f'paid_orders:{order_id}', 'amount_paid')
-    if None in order:
-        return {'Error': 'Order not found'}, 404
+    order = paid_order_colleciton.find_one_or_404({'order_id': order_id})
+    user = user_collection.find_one_or_404({'_id': ObjectId(user_id)})
+    amount = order['amount']
 
-    amount = order[0]
-    p = db.pipeline(transaction=True)
-    p.hincrby(f'user:{user_id}', 'credit', amount)
-    p.delete(f'paid_orders:{order_id}')
-    p.execute()
+    user['credit'] += amount
+
+    user_collection.update_one(user)
+    paid_order_colleciton.delete_one({'order_id': order_id})
     return 'Success', 200
 
 
 @app.post('/status/<user_id>/<order_id>')
 def payment_status(user_id: str, order_id: str):
-    user = db.exists(f'user:{user_id}')
-    if not user:
-        return {'Error': 'User not found'}, 404
+    order = paid_order_colleciton.find_one({'order_id': order_id})
+    return order is not None
 
-    paid_order = db.exists(f'paid_orders:{order_id}')
-    return {'paid': paid_order > 0}, 200
