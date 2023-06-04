bac0b4b8827bd19485e24cf921f468d8291da9f3
Leon Kempen <l.m.kempen@outlook.com>
2023-06-04 13:57:18 +0200

    k8s migration WIP

---
+++ database_setup_k8s.sh
@@ -0,0 +1,42 @@
+Green='\033[0;32m'
+Color_Off='\033[0m'
+
+ECHO -e "${Green} Setting up shards for the databases ${Color_Off}" > `tty`
+ECHO -e "${Green} Setting up shards for order database.... ${Color_Off}" > `tty`
+#TODO Convert commands below to k8s
+# docker-compose exec -T order_configserver01 sh -c "mongosh < order/scripts/init-configserver.js"
+# docker-compose exec -T order_shard01-a sh -c "mongosh < /order/scripts/init-shard01.js"
+# docker-compose exec -T order_shard02-a sh -c "mongosh < order/scripts/init-shard02.js"
+# docker-compose exec -T order_shard03-a sh -c "mongosh < order/scripts/init-shard03.js"
+
+ECHO -e "${Green} Setting up shards for payment database.... ${Color_Off}" > `tty`
+pc01=$(kubectl get pods -o=name | grep payment-configsvr01 | sed "s/^.\{4\}//")
+kubectl exec --tty $pc01 -- sh -c "mongosh --eval '`cat ./payment/scripts/init-configserver.js`'"
+ 
+ps01=$(kubectl get pods -o=name | grep payment-shard01-a | sed "s/^.\{4\}//")
+kubectl exec --tty $ps01 -- sh -c "mongosh --eval '`cat ./payment/scripts/init-shard01.js`'"
+
+ps02=$(kubectl get pods -o=name | grep payment-shard02-a | sed "s/^.\{4\}//")
+kubectl exec --tty $ps02 -- sh -c "mongosh --eval '`cat ./payment/scripts/init-shard02.js`'"
+
+ps03=$(kubectl get pods -o=name | grep payment-shard03-a | sed "s/^.\{4\}//")
+kubectl exec --tty $ps03 -- sh -c "mongosh --eval '`cat ./payment/scripts/init-shard03.js`'"
+
+ECHO -e "${Green} Done setting up shards for the databases! ${Color_Off}" > `tty`
+
+# Wait for config server and shards to elect primaries
+sleep 20
+
+ECHO -e "${Green} Setting up routers for the database ${Color_Off}" > `tty`
+
+# # # Initializing the router
+# # #Enable sharding and setup sharding-key
+ECHO -e "${Green} Setting the router for the order servive..... ${Color_Off}" > `tty`
+# TODO add k8s version of command below
+# docker-compose exec -T order_router01 sh -c "mongosh < order/scripts/init-router.js"
+
+pr01=$(kubectl get pods -o=name | grep payment-router01 | sed "s/^.\{4\}//")
+kubectl exec --tty $pr01 -- sh -c "mongosh --eval '`cat ./payment/scripts/init-router.js`'"
+kubectl exec --tty $pr01 -- sh -c "mongosh --eval '`cat ./payment/scripts/init-mongo.js`'"
+ECHO -e "${Green} Done settting up the routers! ${Color_Off}" > `tty`
+sleep 20
--- payment/scripts/init-shard02.js
+++ payment/scripts/init-shard02.js
@@ -2,8 +2,8 @@ rs.initiate({
   _id: "payment_rs-shard-02",
   version: 1,
   members: [
-    { _id: 0, host: "payment_shard02-a:27017" },
-    { _id: 1, host: "payment_shard02-b:27017" },
-    { _id: 2, host: "payment_shard02-c:27017" },
+    { _id: 0, host: "payment-shard02-a:27125" },
+    { _id: 1, host: "payment-shard02-b:27126" },
+    { _id: 2, host: "payment-shard02-c:27127" },
   ],
 });
