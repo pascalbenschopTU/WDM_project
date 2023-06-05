efded697f1151572b0611331089757581e7b1116
Leon Kempen <l.m.kempen@outlook.com>
2023-06-05 12:23:42 +0200

    Fixed order config script

--- order/scripts/init-configserver.js
+++ order/scripts/init-configserver.js
@@ -4,9 +4,9 @@ rs.initiate(
         configsvr: true, 
         version: 1, 
         members: [
-            { _id: 0, host: "order-configsvr01:27116" },
-            { _id: 1, host: "order-configsvr02:27117" },
-            { _id: 2, host: "order-configsvr03:27118" },
+            { _id: 0, host: "order-configserver01:27116" },
+            // { _id: 1, host: "order-configserver02:27117" },
+            // { _id: 2, host: "order-configserver03:27118" },
           ],
     }
 )
